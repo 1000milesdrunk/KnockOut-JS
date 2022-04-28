b755b20a0a573b6a411959bb9458a8a8e1f296a4
Sudharsan <specsudharsan@gmail.com>
2022-04-28 10:51:48 +0530

    formatted

--- Form/script.js
+++ Form/script.js
@@ -8,75 +8,26 @@ function ViewModal() {
   }, this);
 
   this.currentAddress = ko.observable("");
-  // current = this.currentAddress();
-  // this.permAddress = ko.observable("");
-  // let checkBox = document.querySelector("#checkIt");
-  // checkBox.addEventListener("change", function () {
-  // if (checkValue) {
-  // this.currentAddress = self.currentAddress;
   this.permAddress = ko.computed(function () {
     return self.currentAddress();
   }, self);
-  // console.log("Its checked");
-  // } else {
-  //   console.log("It not checked");
-  //   this.permAddress = ko.observable("");
-  // }
 }
-// this.checkBox = ko.observable(false);
-// this.myAction = ko.computed(function () {
-//   return true;
-// }, this);
-// let check_Box = this.checkBox;
-// if (this.checkBox) {
-//   console.log("Its checked");
-// } else {
-//   console.log("its not checked");
-// }
-
-var model1 = new ViewModal();
 //activate knockout js
+var model1 = new ViewModal();
 ko.applyBindings(model1);
 
 //add event listener outside
-
-// let checkBox = document.querySelector("#checkIt");
-// checkBox.addEventListener("change", function () {
-//   if (this.checked) {
-//     // console.log(self);
-//     ViewModal(this.checked);
-//     // model1.currentAddress = self.currentAddress();
-//     // model1.permAddress = ko.computed(function () {
-//     //   return self.currentAddress();
-//     // }, self);
-//     // console.log(model1.currentAddress());
-//     // console.log(model1.permAddress());
-//     // console.log("Its checked");
-//   } else {
-//     // model1.permAddress = ko.observable("");
-//     console.log("It not checked");
-//   }
-// });
-
 let checkBox = document.querySelector("#checkIt");
-checkBox.addEventListener("change", function (event) {
+checkBox.addEventListener("change", function () {
   if (!checkBox.checked) {
     document.querySelector(".pAddress").value = "";
-    // document.getElementById("pAddress").setAttribute("disabled", false);
     document.getElementById("pAddress").toggleAttribute("disabled");
   } else {
     document.querySelector(".pAddress").value = model1.currentAddress();
-    // document.getElementById("pAddress").setAttribute("disabled", true);
-    // document.getElementById("pAddress").removeAttribute("disabled");
     document.getElementById("pAddress").toggleAttribute("disabled");
   }
 });
-
-// document.querySelector("button").addEventListener("click", function () {
-//   let userDetails = document.querySelector("input").value;
-//   console.log(userDetails);
-// });
-
+const details1 = [];
 function display() {
   let userDetails = {};
   userDetails.firstName = document.getElementById("fname").value;
@@ -85,7 +36,6 @@ function display() {
   userDetails.currentAddress = document.getElementById("cAddress").value;
   userDetails.permanentAddress = document.getElementById("pAddress").value;
   console.log(userDetails);
-  const details1 = [];
   details1.push(JSON.stringify(userDetails));
   console.log(details1);
 }
