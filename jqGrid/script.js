804addec9a24d811f01f9727d8c66ad6a12615a3
Sudharsan <specsudharsan@gmail.com>
2022-04-29 12:49:56 +0530

    jqGrid

--- jqGrid/script.js
+++ jqGrid/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+$(function () {
+  $("#jqGrid").jqGrid({
+    colModel: [{ name: "firstName" }, { name: "lastName" }],
+    data: [
+      { id: 10, firstName: "Angela", lastName: "Merkel" },
+      { id: 20, firstName: "Vladimir", lastName: "Putin" },
+      { id: 30, firstName: "David", lastName: "Cameron" },
+      { id: 40, firstName: "Barack", lastName: "Obama" },
+      { id: 50, firstName: "François", lastName: "Hollande" },
+    ],
+  });
+});
